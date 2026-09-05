eea26c5444bac2917772671f07742c916fe95302
mapdamamap <ballola.pamela@gmail.com>
2026-09-05 17:18:59 +0800

    Fixed UI

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ 
                 success: true, 
-                message: `File uploaded successfully: ${uploadedFile.newFilename}` 
+                message: `File uploaded successfully!` 
             }));
         });
         return;
